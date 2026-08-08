4db824a93671e61d7323d643fef5acfba7c0415f
Rehan Khan <rehan99211@gmail.com>
2026-08-08 08:00:47 +0530

    Update price of Dal Makhani to 280

--- Practice-react/src/components/menuApi.js
+++ Practice-react/src/components/menuApi.js
@@ -69,7 +69,7 @@
     "name": "Dal Makhani",
     "image": "https://example.com/images/dal-makhani.jpg",
     "category": "dinner",
-    "price": 270,
+    "price": 280,
     "description": "Slow-cooked black lentils in creamy tomato gravy."
   },
   {
