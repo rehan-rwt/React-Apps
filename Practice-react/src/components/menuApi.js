de6e56c548c653bb89f09ebc3799d24b68d531f6
Rehan Khan <rehan99211@gmail.com>
2026-08-22 13:42:28 +0530

    Done
    Updated the price of Paneer Tikka from 290 to 280.

--- Practice-react/src/components/menuApi.js
+++ Practice-react/src/components/menuApi.js
@@ -13,7 +13,7 @@
     "name": "Paneer Tikka",
     "image": "images/paneertikka.jpg",  
     "category": "lunch",
-    "price": 290,
+    "price": 280,
     "description": "Grilled paneer cubes marinated in yogurt and Indian spices."
   },
   {
