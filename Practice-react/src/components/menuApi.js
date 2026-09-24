b18ab3e3b217a485074dad83c475f46931b31e16
Rehan Khan <rehan99211@gmail.com>
2026-09-24 13:28:28 +0530

    Update menuApi.js

--- Practice-react/src/components/menuApi.js
+++ Practice-react/src/components/menuApi.js
@@ -5,7 +5,7 @@
     "name": "Butter Chicken",
     "image": "https://example.com/images/butter-chicken.jpg",
     "category": "dinner",
-    "price": 370,
+    "price": 380,
     "description": "Creamy butter chicken cooked with aromatic spices and served with naan."
   },
   {
